1e6f9b19891a8c898f2e0e0fca811ac21ed27067
louie-sage <Louie.Sage@adas.com>
2026-09-18 15:48:09 +0100

    adding cover crops controller

---
+++ FrameAgricultureApi/Controllers/CoverCropsController.cs
@@ -0,0 +1,34 @@
+using System.Net.Mime;
+using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrameAgricultureApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoverCropsController : ControllerBase
+    {
+        
+        [Consumes(MediaTypeNames.Application.Json)]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(StandardOutput), StatusCodes.Status200OK)]
+        [HttpPost("cover-crop-emissions")]
+        [ApiVersion(1.0)]
+        public IActionResult CoverCropEmissions([FromBody] CoverCropInputs jsonInputObject)
+        {
+            try
+            {
+                StandardOutput standardOutput = new();
+                if(jsonInputObject != null)
+                {
+                    CoverCropEmissions emissions = CoverCrops.CoverCropEmissions(jsonInputObject.CoverCropType);
+                    standardOutput = new StandardOutput("Cover Crops", jsonInputObject.CropType.ToString(), "Crop Production", emissions.ExportEmissions());
+                }
+                return Ok(standardOutput);
+            }
+            catch(Exception ex) { return BadRequest(ex.Message); }
+        }
+
+    }
+}
