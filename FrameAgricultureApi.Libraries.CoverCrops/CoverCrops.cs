47fc853a929fecb910e5c6b5296d4df9a91ac947
louie-sage <Louie.Sage@adas.com>
2026-09-21 10:32:28 +0100

    adding crop look up to the cover crop emission

--- FrameAgricultureApi.Libraries.CoverCrops/CoverCrops.cs
+++ FrameAgricultureApi.Libraries.CoverCrops/CoverCrops.cs
@@ -1,5 +1,6 @@
 using System;
 using FrameAgricultureApi.Generic;
+using FrameAgricultureApi.Libraries.LookUps;
 
 namespace FrameAgricultureApi.Libraries.CoverCrops;
 
@@ -19,18 +20,18 @@ public class CoverCrops
         CoverCropEmissions myEmissions = new();
         myEmissions.InitialiseCoverCropEmissions();
 
-        double additionalNReturn = 10;//CropLookup.RetrieveAdditionalNReturn_CoverCrop(coverCropType);
+        double additionalNReturn = CropLookup.RetrieveAdditionalNReturn_CoverCrop(coverCropType);
         myEmissions.additionalOutputs[AdditionalCoverCropEmissions.Nreturn].Value = additionalNReturn;
 
-        myEmissions.EmissionsCore[CoreEmissions.DirectN2ON].Value = GenericEquations.Emission_PercentageEF(additionalNReturn, 10); // GenericEquations.Emission_PercentageEF(additionalNReturn, CropLookup.RetrieveIPCCEmissionFactor1());
-        double adjustedFracLeach = 10;// CropLookup.RetrieveFracLeach() * ((100 - CropLookup.RetrievedCoverCropFracLeachReduction()) * HelperFunctions.Percent_to_Proportion);
+        myEmissions.EmissionsCore[CoreEmissions.DirectN2ON].Value = GenericEquations.Emission_PercentageEF(additionalNReturn, CropLookup.RetrieveIPCCEmissionFactor1());
+        double adjustedFracLeach = CropLookup.RetrieveFracLeach() * ((100 - CropLookup.RetrievedCoverCropFracLeachReduction()) * HelperFunctions.Percent_to_Proportion);
         myEmissions.EmissionsCore[CoreEmissions.LeachedNO3N].Value = GenericEquations.Emission_PercentageEF(additionalNReturn, adjustedFracLeach);
-        myEmissions.additionalOutputs[AdditionalCoverCropEmissions.unadjustedNO3N].Value = GenericEquations.Emission_PercentageEF(additionalNReturn, 10);// GenericEquations.Emission_PercentageEF(additionalNReturn, CropLookup.RetrieveFracLeach());
-        myEmissions.EmissionsCore[CoreEmissions.N2ONleached].Value = GenericEquations.Emission_PercentageEF(myEmissions.EmissionsCore[CoreEmissions.LeachedNO3N].Value, 10); // GenericEquations.Emission_PercentageEF(myEmissions.EmissionsCore[CoreEmissions.LeachedNO3N].Value, CropLookup.RetrieveIPCCEmissionFactor5());
-        myEmissions.additionalOutputs[AdditionalCoverCropEmissions.unadjustedN2ONLeach].Value = GenericEquations.Emission_PercentageEF(myEmissions.additionalOutputs[AdditionalCoverCropEmissions.unadjustedNO3N].Value, 10); //GenericEquations.Emission_PercentageEF(myEmissions.additionalOutputs[AdditionalCoverCropEmissions.unadjustedNO3N].Value, CropLookup.RetrieveIPCCEmissionFactor5());
-        myEmissions.EmissionsCore[CoreEmissions.DirectNON].Value = GenericEquations.Emission_ProportionEF(myEmissions.EmissionsCore[CoreEmissions.DirectN2ON].Value, 10); //GenericEquations.Emission_ProportionEF(myEmissions.EmissionsCore[CoreEmissions.DirectN2ON].Value, CropLookup.RetrieveRatioNON());
-        myEmissions.EmissionsCore[CoreEmissions.DirectN2N].Value = GenericEquations.Emission_ProportionEF(myEmissions.EmissionsCore[CoreEmissions.DirectN2ON].Value, 10); //GenericEquations.Emission_ProportionEF(myEmissions.EmissionsCore[CoreEmissions.DirectN2ON].Value, CropLookup.RetrieveRatioN2N());
-        myEmissions.EmissionsCore[CoreEmissions.N2ONvolatalised].Value = GenericEquations.Emission_PercentageEF(myEmissions.EmissionsCore[CoreEmissions.DirectNON].Value, 10); //GenericEquations.Emission_PercentageEF(myEmissions.EmissionsCore[CoreEmissions.DirectNON].Value, CropLookup.RetrieveIPCCEmissionFactor4());
+        myEmissions.additionalOutputs[AdditionalCoverCropEmissions.unadjustedNO3N].Value = GenericEquations.Emission_PercentageEF(additionalNReturn, CropLookup.RetrieveFracLeach());
+        myEmissions.EmissionsCore[CoreEmissions.N2ONleached].Value = GenericEquations.Emission_PercentageEF(myEmissions.EmissionsCore[CoreEmissions.LeachedNO3N].Value, CropLookup.RetrieveIPCCEmissionFactor5());
+        myEmissions.additionalOutputs[AdditionalCoverCropEmissions.unadjustedN2ONLeach].Value = GenericEquations.Emission_PercentageEF(myEmissions.additionalOutputs[AdditionalCoverCropEmissions.unadjustedNO3N].Value, CropLookup.RetrieveIPCCEmissionFactor5());
+        myEmissions.EmissionsCore[CoreEmissions.DirectNON].Value = GenericEquations.Emission_ProportionEF(myEmissions.EmissionsCore[CoreEmissions.DirectN2ON].Value, CropLookup.RetrieveRatioNON());
+        myEmissions.EmissionsCore[CoreEmissions.DirectN2N].Value = GenericEquations.Emission_ProportionEF(myEmissions.EmissionsCore[CoreEmissions.DirectN2ON].Value, CropLookup.RetrieveRatioN2N());
+        myEmissions.EmissionsCore[CoreEmissions.N2ONvolatalised].Value = GenericEquations.Emission_PercentageEF(myEmissions.EmissionsCore[CoreEmissions.DirectNON].Value, CropLookup.RetrieveIPCCEmissionFactor4());
 
         return myEmissions;
     }
